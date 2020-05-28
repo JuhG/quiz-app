154c83fd182bbcbba3fe65d9c7a98caad5be67df
JuhG <juhg24@gmail.com>
2020-05-28 23:35:02 +0200

    make game mechanic work

--- src/features/game/Quiz.js
+++ src/features/game/Quiz.js
@@ -1,32 +1,83 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectQuestions } from '../questions/questionsSlice'
+import { changeCurrent } from './gameSlice'
 import styles from './Quiz.module.css'
 
 export default (props) => {
+  const dispatch = useDispatch()
+
   const questions = useSelector(selectQuestions)
-  const [current, setCurrent] = useState(0)
+  const [step, setStep] = useState(0)
   const [points, setPoints] = useState(0)
+  const [selected, setSelected] = useState(null)
+
+  const getColor = (answer, selected) => {
+    if (answer.id === selected) {
+      if (answer.correct) {
+        return 'green'
+      } else {
+        return 'red'
+      }
+    }
+
+    if (answer.correct) {
+      return 'lightgreen'
+    }
+  }
 
   return (
     <div {...props}>
       <div className={styles.wrapper}>
         <div className={styles.header}>
-          <p>{`${current + 1} / ${questions.length}`}</p>
+          <p>{`${step + 1} / ${questions.length}`}</p>
           <p>Points: {points}</p>
         </div>
 
-        <h2 className={styles.title}>{questions[current].question}</h2>
+        <h2 className={styles.title}>{questions[step].question}</h2>
 
         <ul className={styles.list}>
-          {questions[current].answers.map((answer) => {
+          {questions[step].answers.map((answer) => {
             return (
               <li className={styles.item} key={answer.id}>
-                <button className={styles.button}>{answer.text}</button>
+                <button
+                  style={{
+                    background:
+                      null === selected ? '' : getColor(answer, selected),
+                  }}
+                  className={styles.button}
+                  onClick={() => {
+                    if (null !== selected) return
+                    setSelected(answer.id)
+                    if (answer.correct) {
+                      setPoints(points + 1)
+                    }
+                  }}
+                >
+                  {answer.text}
+                </button>
               </li>
             )
           })}
         </ul>
+
+        {null === selected ? null : (
+          <div className={styles.nav}>
+            <button
+              onClick={() => {
+                if (step + 1 === questions.length) {
+                  dispatch(changeCurrent('IDLE'))
+                  return
+                }
+
+                setSelected(null)
+                setStep(step + 1)
+              }}
+            >
+              {step + 1 === questions.length ? 'Finish' : 'Next'}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
