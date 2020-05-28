6cc91c5bcbe73bc0d355cc29c1a4db582f7a4b33
JuhG <juhg24@gmail.com>
2020-05-29 00:37:03 +0200

    styling questions

--- src/features/questions/Questions.js
+++ src/features/questions/Questions.js
@@ -1,5 +1,6 @@
-import React, { useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import styles from './Questions.module.css'
 import { add, remove, selectQuestions } from './questionsSlice'
 
 const Questions = (props) => {
@@ -7,78 +8,97 @@ const Questions = (props) => {
   const dispatch = useDispatch()
   const [form, setForm] = useState({})
 
-  const handleChange = useMemo(
-    () => (event) => {
-      event.persist()
-
-      setForm((form) => ({
-        ...form,
-        [event.target.name]: event.target.value,
-      }))
-    },
-    []
-  )
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    })
+  }
 
   return (
     <div {...props}>
-      <ul>
+      <ul className={styles.list}>
         {questions.map((item) => (
-          <li key={item.id}>
-            <p>{item.question}</p>
-            <ul>
+          <li key={item.id} className={styles.item}>
+            <div className={styles.savedHeader}>
+              <h3>{item.question}</h3>
+              <button
+                onClick={() => {
+                  if (
+                    window.confirm(
+                      'Do you really want to delete this question?'
+                    )
+                  ) {
+                    dispatch(remove(item.id))
+                  }
+                }}
+              >
+                Delete
+              </button>
+            </div>
+
+            <ul className={styles.savedAnswers}>
               {item.answers.map((answer) => (
-                <li key={answer.id}>{answer.text}</li>
+                <li key={answer.id} className={styles.savedAnswer}>
+                  <p>
+                    <span>{answer.text}</span>
+                    {answer.correct ? (
+                      <span className={styles.correct} />
+                    ) : null}
+                  </p>
+                </li>
               ))}
             </ul>
-            <button
-              onClick={() => {
-                if (
-                  window.confirm('Do you really want to delete this question?')
-                ) {
-                  dispatch(remove(item.id))
-                }
-              }}
-            >
-              Delete
-            </button>
           </li>
         ))}
       </ul>
+
       <form
+        className={styles.form}
         onSubmit={(e) => {
           e.preventDefault()
           // no "server" side validation
           dispatch(add(form))
+          setForm({})
+          console.log(form, form['question'])
         }}
       >
-        <input
-          required
-          type="text"
-          maxLength="300"
-          name="question"
-          onChange={handleChange}
-        />
+        <div className={styles.questionHeader}>
+          <h2>Add a new question</h2>
+          <input
+            required
+            type="text"
+            maxLength="300"
+            name="question"
+            value={form['question'] || ''}
+            onChange={handleChange}
+          />
+          <button type="submit">Add</button>
+        </div>
 
-        {[1, 2, 3, 4].map((number) => (
-          <div key={number}>
-            <input
-              required
-              type="text"
-              maxLength="300"
-              name={`answer-${number}`}
-              onChange={handleChange}
-            />
-            <input
-              required
-              type="radio"
-              name="correct"
-              value={number}
-              onChange={handleChange}
-            />
-          </div>
-        ))}
-
-        <button type="submit">Add</button>
+        <ul className={styles.answers}>
+          {[1, 2, 3, 4].map((number) => (
+            <div key={number} className={styles.answer}>
+              <p>Answer #{number}</p>
+              <input
+                required
+                type="text"
+                maxLength="300"
+                name={`answer-${number}`}
+                value={form[`answer-${number}`] || ''}
+                onChange={handleChange}
+              />
+              <input
+                required
+                type="radio"
+                name="correct"
+                value={number}
+                checked={Number(form['correct']) === number}
+                onChange={handleChange}
+              />
+            </div>
+          ))}
+        </ul>
       </form>
     </div>
   )
